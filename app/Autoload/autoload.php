3359554cef23d02c1241f55e25e924b3463afed9
Xawier Slupik <48760619+xBlixon@users.noreply.github.com>
2023-05-26 18:56:19 +0200

    Removed unsued autoloader

--- app/Autoload/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-function autoload(string $classname)
-{
-    $classname = str_replace("\\", OS_SLASH, $classname);
-    require _APP . $classname . ".php"; // equivalent of /app/Elysio/.../class.php
-}
-
-spl_autoload_register('autoload');
